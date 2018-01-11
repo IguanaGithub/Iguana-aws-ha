78b70a5f35fe05941acc2cb731363e0437a3ebf0
richard <richard.wang@interfaceware.com>
2018-01-11 15:54:21 +0000

    Export from The Fake: test

--- HADemo-HTTP_Auto_Deploy-FromHTTP-TVq5y1EAJvIlWR/main.lua
+++ HADemo-HTTP_Auto_Deploy-FromHTTP-TVq5y1EAJvIlWR/main.lua
@@ -1,5 +1,6 @@
 -- The main function is the first function called from Iguana.
 -- The Data argument will contain the message to be processed.
 function main(Data)
-   net.http.respond{body="OK"}
+   net.http.respond{body="OK2"}
+   
 end
